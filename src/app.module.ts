42ad2df1b961724b781872f5886ef48012e59d67
codewithumar <codewithumar@gmail.com>
2024-12-26 19:53:35 +0500

    fix .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,19 @@ import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot(
-    "mongodb+srv://Cluster56258:92cF3_xzURvgnMy@cluster56258.d5lxj.mongodb.net/test"
-  ), AuthModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env', 
+      isGlobal: true, 
+    }),
+    MongooseModule.forRoot(
+      process.env.DB_URL,
+    ), 
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
