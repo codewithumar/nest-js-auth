4e2f5ac8a1c5373f4b641c0251a01a677ff05921
codewithumar <codewithumar@gmail.com>
2024-12-27 02:04:38 +0500

    code clean: remove unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     AuthModule,
